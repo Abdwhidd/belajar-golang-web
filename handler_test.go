266a8257abceb6f05d84c70130ad50d8be2882e8
Wahid <abdulwahidkaharr@gmail.com>
2023-10-11 15:46:16 +0700

    handler

---
+++ handler_test.go
@@ -0,0 +1,25 @@
+package belajar_golang_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+
+	var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request) {
+		// Logic web
+		fmt.Fprint(writer, "Hello Wahid")
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
